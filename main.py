e0982e86084ddd9c1b06a1b7559c0fb6c5436b9a
Lexain_ii <84253380+Olalekan1501@users.noreply.github.com>
2022-05-28 00:30:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 # Example:
 # find_anagrams("hello", "check") --> False
 # find_anagrams("below", "elbow") --> True
-word=input("first_word: ")
-anagram=input("second_word: ")
+from collections import Counter
 
-def find_anagram():
-    # [assignment] Add your code here
-    if sorted(word)== sorted(anagram)                                                                                                                                                                                       :
-         return("the anagram is true")
+def find_anagram(a, b):
+    # implenting counter function
+    if sorted(word) == sorted(anagram):                                                                                                                                                                                       :
+         print("The anagram is true")
     else:
-         return("the anagram is false")
-
-find_anagram()
+         print("The anagram is false")
+            
+a = "earth"
+b = "heart"
+find_anagram(a, b)
